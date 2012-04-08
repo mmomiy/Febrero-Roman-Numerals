41ecc20d316076b8b13ad2906095693e1ff992a6
Monica Momiy <mmomiy@gmail.com>
2012-04-08 09:53:10 -0500

    segundo test - no convertir 0

--- mmomiy/romannumerals.rb
+++ mmomiy/romannumerals.rb
@@ -1,7 +1,11 @@
 class RomanNumerals
 
 	def convertir_romano(numeronatural)
+		if numeronatural == 0
+			raise "No se puede convertir el numero 0"
+		end
 		return "I"
 	end
-	
+
 end
+
--- mmomiy/romannumerals_spec.rb
+++ mmomiy/romannumerals_spec.rb
@@ -2,12 +2,23 @@ require './romannumerals.rb'
 
 describe RomanNumerals do
 
+  before(:each)do
+    @romano = RomanNumerals.new()
+  end
+
+  it "no debe ser 0" do
+    expect {
+        @romano.convertir_romano(0)
+      }.should raise_exception
+  end
+
   context "convertir " do
 
     it "1 en I" do
-      @romano = RomanNumerals.new()   
-      @romano.convertir_romano(1).should == "I"
+     @romano.convertir_romano(1).should == "I"
     end
   end
+
   
 end
+
